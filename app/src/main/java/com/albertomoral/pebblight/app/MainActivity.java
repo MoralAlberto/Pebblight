70a531c62e04f735575628e183d20f39b4f70963
micwallace <micwallace@gmx.com>
2015-04-07 23:42:05 +1000

    Added service and broadcast receiver to allow light toggle when the app isn't open.

--- app/src/main/java/com/albertomoral/pebblight/app/MainActivity.java
+++ app/src/main/java/com/albertomoral/pebblight/app/MainActivity.java
@@ -4,18 +4,12 @@ import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.hardware.Camera.Parameters;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import com.albertomoral.pebblight.app.PebbleKit.*;
-import com.albertomoral.pebblight.app.util.PebbleDictionary;
 
 import java.util.UUID;
 
@@ -24,20 +18,14 @@ public class MainActivity extends Activity {
 
     private final static UUID PEBBLE_APP_UUID = UUID.fromString("b9152c98-067a-44e2-8ad2-9fb548d6c480");
 
-    //flag to detect flash is on or off
-    private boolean isLighOn = false;
-    private Camera camera;
-
-    private Button button;
     ImageView image;
+    LightReceiver lightReceiver;
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        if (camera != null) {
-            camera.release();
-        }
+        unregisterReceiver(lightReceiver);
     }
 
     @Override
@@ -45,116 +33,42 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.buttonFlashlight);
+        Button button = (Button) findViewById(R.id.buttonFlashlight);
         image = (ImageView) findViewById(R.id.background);
 
 
-        Context context = this;
-        PackageManager pm = context.getPackageManager();
-
-        // if device support camera?
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            Log.e("err", "Device has no camera!");
-            return;
-        }
-
-        camera = Camera.open();
-        final Parameters p = camera.getParameters();
+        final Context context = this;
 
         button.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
 
-                if (isLighOn) {
-
-                    Log.i("info", "torch is turn off!");
-
-                    boolean connected = PebbleKit.isWatchConnected(getApplicationContext());
-                    Log.e(getLocalClassName(), "Pebble is " + (connected ? "connected" : "not connected"));
-
-
-                    p.setFlashMode(Parameters.FLASH_MODE_OFF);
-                    camera.setParameters(p);
-                    camera.stopPreview();
-                    isLighOn = false;
-
-
-                    image.setImageResource(R.drawable.ic_fondo_off);
-
-
-                } else {
-
-                    Log.i("info", "torch is turn on!");
-
-                    p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-
-                    camera.setParameters(p);
-                    camera.startPreview();
-                    isLighOn = true;
-
-
-                    image.setImageResource(R.drawable.ic_fondo_on);
-
-                }
-
+                LightService.toggleLight(context);
             }
         });
+
+        // Setup Service broadcast listener
+        lightReceiver = new LightReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(LightService.ACTION_LIGHT_STATUS);
+        registerReceiver(lightReceiver, intentFilter);
 
         // Launching my app
         PebbleKit.startAppOnPebble(getApplicationContext(), PEBBLE_APP_UUID);
 
-        PebbleKit.registerPebbleConnectedReceiver(getApplicationContext(), new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Log.i(getLocalClassName(), "Pebble connected!");
+    }
+
+    private class LightReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getBooleanExtra("lightOn", false)){
+                image.setImageResource(R.drawable.ic_fondo_on);
+            } else {
+                image.setImageResource(R.drawable.ic_fondo_off);
             }
-        });
-
-        PebbleKit.registerPebbleDisconnectedReceiver(getApplicationContext(), new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                Log.i(getLocalClassName(), "Pebble disconnected!");
-            }
-        });
-
-        PebbleKit.registerReceivedDataHandler(this, new PebbleKit.PebbleDataReceiver(PEBBLE_APP_UUID) {
-            @Override
-            public void receiveData(final Context context, final int transactionId, final PebbleDictionary data) {
-                //Log.i(getLocalClassName(), "Received value=" + data.getUnsignedInteger(0) + " for key: 0");
-                if (isLighOn) {
-
-                    Log.i("info", "torch is turn off!");
-
-                    boolean connected = PebbleKit.isWatchConnected(getApplicationContext());
-                    Log.e(getLocalClassName(), "Pebble is " + (connected ? "connected" : "not connected"));
-
-
-                    p.setFlashMode(Parameters.FLASH_MODE_OFF);
-                    camera.setParameters(p);
-                    camera.stopPreview();
-                    isLighOn = false;
-
-                    image.setImageResource(R.drawable.ic_fondo_off);
-
-
-                } else {
-
-                    Log.i("info", "torch is turn on!");
-
-                    p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-
-                    camera.setParameters(p);
-                    camera.startPreview();
-                    isLighOn = true;
-
-                    image.setImageResource(R.drawable.ic_fondo_on);
-
-                }
-
-                PebbleKit.sendAckToPebble(getApplicationContext(), transactionId);
-            }
-        });
+        }
 
     }
 }
