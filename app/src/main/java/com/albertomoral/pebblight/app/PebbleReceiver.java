b69b90e5690983d3c8b38d3e7ccc8110c819f684
micwallace <micwallace@gmx.com>
2015-04-19 15:29:22 +1000

    Fixed issue with intent filtering. Causing light to turn on when other apps are used :-O

--- app/src/main/java/com/albertomoral/pebblight/app/PebbleReceiver.java
+++ app/src/main/java/com/albertomoral/pebblight/app/PebbleReceiver.java
@@ -3,12 +3,18 @@ package com.albertomoral.pebblight.app;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+
+import java.util.UUID;
 
 public class PebbleReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        UUID uuid = (UUID) intent.getExtras().getSerializable(Constants.APP_UUID);
+        if (!uuid.equals(Constants.PEBBLIGHT_UUID))
+            return;
         LightService.toggleLight(context);
         Log.i("PebbleReceiver", "Light toggle request received");
     }
